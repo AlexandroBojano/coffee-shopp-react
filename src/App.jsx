646b4a2963f40b0807b9c1b5c3e92d96f7e74799
Alexandro Bojano <bojanoalexandro@gmail.com>
2025-10-16 15:24:00 -0300

    Navbar tamplete one with grid

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,22 @@ import '../src/css/Variables.css'
 import '../src/css/App.css'
 import '../src/css/Responsive.css'
 import Header from './components/Header'
+import Navbar from "../src/components/Navbar"
 
 function App() {
 
   return (
+
+    
     <>
+
+    <div className="container">
       <Header />
+      <Navbar />
+     
+
+    </div>
+      
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import "../../src/css/Navbar.css"
+
+const Navbar = () => {
+    return (
+        <aside>
+            <h1>nav</h1>
+        </aside>
+    )
+}
+
+export default Navbar
