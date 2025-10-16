2d39f179c78e7938ca3f29da3008140bb7b67162
Alexandro Bojano <bojanoalexandro@gmail.com>
2025-10-16 15:04:52 -0300

    Header finished

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import '../src/css/Reset.css'
+import '../src/css/Fonts.css'
+import '../src/css/Variables.css'
+import '../src/css/App.css'
+import '../src/css/Responsive.css'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+
+import "../css/Header.css"
+const Header = () => {
+
+    return (
+
+       <header>
+
+        <h1>logo</h1>
+
+
+        <nav>
+            <ul className="navegation">
+                <li><a href="">Contatos</a></li>
+                <li><a href="">Sobre Nós</a></li>
+                <li><a href="">Reclamações</a></li>
+            </ul>
+        </nav>
+        
+        
+       </header>
+
+    )
+
+   
+
+}
+
+
+export default Header
